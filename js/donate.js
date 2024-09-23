3ce603fd1481ea4c7fa6638c62c7231e9b8e8479
utshasaha16 <utsharoy12345@gmail.com>
2024-09-24 02:05:29 +0600

    donation and history button functonality

--- js/donate.js
+++ js/donate.js
@@ -1,66 +1,93 @@
 // input-field-one
-document.getElementById('input-btn-one')
-    .addEventListener('click', function(event){
-        event.preventDefault();
-        const addMoney = getInputFieldValue('input-field-one');
-        
-        if(addMoney <= 0 || isNaN(addMoney)){
-            document.getElementById('error-input-field-one').classList.remove('hidden');
-            return;
-        }
+document
+  .getElementById("input-btn-one")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValue("input-field-one");
 
-        const inputOneTotalAmount =getTextFieldValue ('input-one-total-amount');
-        const totalAmount = addMoney + inputOneTotalAmount;
-        document.getElementById('input-one-total-amount').innerText = totalAmount;
+    if (addMoney <= 0 || isNaN(addMoney)) {
+      document
+        .getElementById("error-input-field-one")
+        .classList.remove("hidden");
+      return;
+    }
 
-        const mainTotalBalance = getTextFieldValue('main-balance');
-        const mainBalance = mainTotalBalance - addMoney;
-        document.getElementById('main-balance').innerText = mainBalance;
-        
-    });
+    const inputOneTotalAmount = getTextFieldValue("input-one-total-amount");
+    const totalAmount = addMoney + inputOneTotalAmount;
+    document.getElementById("input-one-total-amount").innerText = totalAmount;
+
+    const mainTotalBalance = getTextFieldValue("main-balance");
+    const mainBalance = mainTotalBalance - addMoney;
+    document.getElementById("main-balance").innerText = mainBalance;
+  });
 // input-field-two
-document.getElementById('input-btn-two')
-    .addEventListener('click', function(event){
-        event.preventDefault();
-        const addMoneyInputTwo = getInputFieldValue('input-field-two');
-        if(addMoneyInputTwo <= 0 || isNaN(addMoneyInputTwo)){
-            document.getElementById('error-input-field-two').classList.remove('hidden');
-            return;
-        }
+document
+  .getElementById("input-btn-two")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoneyInputTwo = getInputFieldValue("input-field-two");
+    if (addMoneyInputTwo <= 0 || isNaN(addMoneyInputTwo)) {
+      document
+        .getElementById("error-input-field-two")
+        .classList.remove("hidden");
+      return;
+    }
 
-        const inputTwoTotalAmount = getTextFieldValue('input-two-total-amount');
-        const accountBalance = addMoneyInputTwo + inputTwoTotalAmount;
+    const inputTwoTotalAmount = getTextFieldValue("input-two-total-amount");
+    const accountBalance = addMoneyInputTwo + inputTwoTotalAmount;
 
-        document.getElementById('input-two-total-amount').innerText = accountBalance;
+    document.getElementById("input-two-total-amount").innerText =
+      accountBalance;
 
-        const mainTotalBalance = getTextFieldValue('main-balance');
-        const mainBalance = mainTotalBalance - addMoneyInputTwo;
-        document.getElementById('main-balance').innerText = mainBalance;
-
-    });
+    const mainTotalBalance = getTextFieldValue("main-balance");
+    const mainBalance = mainTotalBalance - addMoneyInputTwo;
+    document.getElementById("main-balance").innerText = mainBalance;
+  });
 // input-field-three
-document.getElementById('input-btn-three')
-    .addEventListener('click', function(event){
-        event.preventDefault();
-        const addMoneyInputThree = getInputFieldValue('input-field-three');
-        if(addMoneyInputThree <= 0 || isNaN(addMoneyInputThree)){
-            document.getElementById('error-input-field-three').classList.remove('hidden');
-            return;
-        }
+document
+  .getElementById("input-btn-three")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoneyInputThree = getInputFieldValue("input-field-three");
+    if (addMoneyInputThree <= 0 || isNaN(addMoneyInputThree)) {
+      document
+        .getElementById("error-input-field-three")
+        .classList.remove("hidden");
+      return;
+    }
 
-        const cardOneTotalAmount = getTextFieldValue('card-three-total-amount');
-        const cardThreeAccountBalance = addMoneyInputThree + cardOneTotalAmount;
-        
-        document.getElementById('card-three-total-amount').innerText = cardThreeAccountBalance;
+    const cardOneTotalAmount = getTextFieldValue("card-three-total-amount");
+    const cardThreeAccountBalance = addMoneyInputThree + cardOneTotalAmount;
 
-        const mainTotalBalance = getTextFieldValue('main-balance');
-        const mainBalance = mainTotalBalance - addMoneyInputThree;
-        document.getElementById('main-balance').innerText = mainBalance;
+    document.getElementById("card-three-total-amount").innerText =
+      cardThreeAccountBalance;
 
-    })
-// click blog button and go to another page
-document.getElementById('home-button')
-    .addEventListener('click', function(){
-        window.location.href = "./index.html"
-})
-    
+    const mainTotalBalance = getTextFieldValue("main-balance");
+    const mainBalance = mainTotalBalance - addMoneyInputThree;
+    document.getElementById("main-balance").innerText = mainBalance;
+  });
+
+
+// history button
+const historyBtn = document.getElementById("history-btn");
+
+historyBtn.addEventListener("click", function () {
+  historyBtn.classList.add("bg-primary");
+  donationBtn.classList.remove("bg-primary");
+  donationBtn.classList.add("bg-gray-100");
+
+  // hide donation section
+  document.getElementById("donation-section").classList.add("hidden");
+});
+// donation button
+const donationBtn = document.getElementById("donation-btn");
+donationBtn.addEventListener("click", function () {
+  
+    historyBtn.classList.remove("bg-primary");
+  donationBtn.classList.add("bg-primary");
+  historyBtn.classList.add("bg-gray-100");
+ 
+
+  // hide donation section
+  document.getElementById("donation-section").classList.remove("hidden");
+});
--- js/utilitys.js
+++ js/utilitys.js
@@ -6,3 +6,13 @@ function getTextFieldValue(id){
     const textNumber = parseFloat(document.getElementById(id).innerText);
     return textNumber;
 }
+
+// click blog button and go to another page
+document.getElementById('blog-button')
+    .addEventListener('click', function(){
+        window.location.href = "/index.html"
+})
+document.getElementById('home-btn')
+    .addEventListener('click', function(){
+        window.location.href = '/home.html'
+});
